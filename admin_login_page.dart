b860056c9cee394d1e264dd98204655d8fc309a3
Minada Amarasinghe <115599493+minababo@users.noreply.github.com>
2025-04-04 08:18:31 +0530

    added admin-login logic

---
+++ admin_login_page.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:knowtocook/pages/admin_dashboard.dart';
+
+class AdminLoginPage extends StatefulWidget {
+  @override
+  _AdminLoginPageState createState() => _AdminLoginPageState();
+}
+
+class _AdminLoginPageState extends State<AdminLoginPage> {
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<void> _adminLogin() async {
+    try {
+      String inputEmail = _emailController.text.trim();
+
+      if (inputEmail == 'admin@gmail.com') {
+        UserCredential userCredential = await _auth.signInWithEmailAndPassword(
+          email: inputEmail,
+          password: _passwordController.text.trim(),
+        );
+
+        if (userCredential.user != null) {
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(builder: (context) => AdminDashboardPage()),
+          );
+        }
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text("Only admin can access this page")),
+        );
+      }
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("Error: $e")),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Admin Login'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: _emailController,
+              decoration: InputDecoration(labelText: 'Email'),
+              keyboardType: TextInputType.emailAddress,
+            ),
+            TextField(
+              controller: _passwordController,
+              decoration: InputDecoration(labelText: 'Password'),
+              obscureText: true,
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _adminLogin,
+              child: Text('Login as Admin'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
